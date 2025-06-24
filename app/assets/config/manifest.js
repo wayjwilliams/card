8d1d5e9537741ec940588f7d4824a659cf20715d
Jared Williams <jaredwilliamsmail@gmail.com>
2025-06-23 21:17:52 -0500

    add app css to manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 //= link_tree ../images
 //= link controllers/application.js
 //= link controllers/hello_controller.js
+//= link application.css
 //= link tailwind.css
 //= link application.js
 //= link controllers/index.js
